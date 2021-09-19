2de5eafbb44526fabe2deeeb2b3648675bced2d3
Kim-MinHyeok <48408314+Kim-MinHyeok@users.noreply.github.com>
2021-09-19 19:37:54 +0900

    화면 출력 방법 수정

--- src/git_test2/Hello.java
+++ src/git_test2/Hello.java
@@ -2,8 +2,17 @@ package git_test2;
 
 public class Hello {
 
-	String message = "æ»≥Á«œººø‰";
-	public void say() {
-		System.out.println(message);
+	String message;
+	
+	public Hello() {
+		this("æ»≥Á«œººø‰");
+	}
+	
+	public Hello(String message) {
+		this.message = message;
+	}
+	
+	public String say() {
+		return message;
 	}
 }
